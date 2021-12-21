f88698032061d2b6e0e2b41a0dc7e7080b5cc497
Jason P <jasonp000@gmail.com>
2021-12-21 19:10:40 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/org420/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket_prefix = prefix
+  bucket_prefix = "JasonP"
 }
 
